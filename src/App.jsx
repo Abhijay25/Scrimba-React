3d03f9f95fe6306dd01263a62848dcf49f1f32b3
Abhijay <abhijay3852@gmail.com>
2025-05-17 17:34:26 +0800

    Organized Compoents into Seperate Folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function ReactList() {
-  return (
-    <ul className="pointList">
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on GitHub</li>
-      <li>Is maintained by Meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  )
-}
-
-function Header() {
-  return (
-    <header className="header">
-      <img src = "/src/assets/react.svg" className="nav-logo" alt = "React logo" />
-      <nav>
-        <ul className = "nav-list">
-          <li className="nav-list-item">Pricing</li>
-          <li className="nav-list-item">About</li>
-          <li className="nav-list-item">Contact</li>
-        </ul>
-      </nav>
-    </header> 
-  )
-}
+import ReactList from "./Components/ReactList.jsx"
+import Header from "./Components/Header.jsx"
+import Footer from "./Components/Footer.jsx"
 
 function Page() {
   return (
-   <>
+   < >
     <Header /> 
-    <h1>Fun Facts About React</h1>
     <ReactList />
-
-    <footer>
-      <small>© 2025 Abhijay development. All rights reserved.</small>
-    </footer>
+    <Footer />
    </>
   )
 }
---
+++ src/Components/ReactList.jsx
@@ -0,0 +1,14 @@
+export default function ReactList() {
+  return (
+    < >
+      <h1>Fun Facts About React</h1>
+      <ul className="pointList">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
+  )
+}
